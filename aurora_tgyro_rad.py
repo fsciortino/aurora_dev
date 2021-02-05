acc6b94fbc004b5fd0899e80fed6d166fb3e1772
Francesco Sciortino <sciortino@psfc.mit.edu>
2021-02-04 18:23:40 -0800

    cooling curves

--- aurora_tgyro_rad.py
+++ aurora_tgyro_rad.py
@@ -1,7 +1,8 @@
-'''Automated plots of total radiation density from Aurora for a number of ions.
-Results in W/cm^3.
+'''Cooling curves for multiple ions from Aurora. 
 
 No charge exchange included. Simple ionization equilibrium.
+
+sciortino, 2021
 '''
 
 import aurora
@@ -9,15 +10,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
 
-plt.style.use('/home/sciortino/SPARC/sparc_plots.mplstyle')
-
-
-
 fig1,ax1 = plt.subplots()
 
-for imp in ['He','N','Ne','Ar','W']:
+for imp in ['He','C','O','Ar','W']:
 
+    # scan Te and fix a value of ne
     Te_eV = np.logspace(np.log10(10), np.log10(1e5), 1000)
     ne_cm3 = 5e13 * np.ones_like(Te_eV)
+    
+    # read atomic data, interpolate and plot cooling factors
+    line_rad_tot, cont_rad_tot = aurora.get_cooling_factors(
+        imp, ne_cm3, Te_eV, show_components=False, ax=ax1
+        )
 
-    out = aurora.get_cooling_factors(imp, ne_cm3, Te_eV, show_components=False, ax=ax1)
+    # NB: total cooling curve is simply cont_rad_tot+line_rad_tot
